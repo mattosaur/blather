aad949738c8e85429211366c8512e42f7ed4c7ff
Matthew Dyer <matthew.b.dyer@gmail.com>
2014-02-14 11:44:58 -0500

    finished chapter 9

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
   
-  before_action :signed_in_user,  only: [:edit, :update, :index]
+  before_action :signed_in_user,  only: [:edit, :update, :index, :destroy]
   before_action :correct_user,    only: [:edit, :update]
+  before_action :admin_user, only: :destroy
 
   def show
   	@user = User.find(params[:id])
@@ -34,26 +35,36 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted."
+    redirect_to users_url
+  end
+
   def index
     @users = User.paginate(page: params[:page])
   end
 
   private
 
-  def user_params
-  	params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
+    def user_params
+    	params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 
-  # Before filters
+    # Before filters
 
-  def signed_in_user
-    store_location
-    redirect_to signin_url, notice: "Please sign in." unless signed_in?
-  end
+    def signed_in_user
+      store_location
+      redirect_to signin_url, notice: "Please sign in." unless signed_in?
+    end
 
-  def correct_user
-    @user = User.find(params[:id])
-    redirect_to(root_url) unless current_user?(@user)
-  end
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
 
 end
