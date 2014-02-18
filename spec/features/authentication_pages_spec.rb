a8be756d74466994dc7d0bb7a475f47f1c364e22
Matthew Dyer <matthew.b.dyer@gmail.com>
2014-02-18 17:33:47 -0500

    Up to listing 10.29.

--- spec/features/authentication_pages_spec.rb
+++ spec/features/authentication_pages_spec.rb
@@ -29,7 +29,7 @@ describe "Authentication" do
    	it { should have_title(user.name) }
    	it { should have_link('Profile',     href: user_path(user)) }
     it { should have_link('Users',       href: users_path) }
-    #it { should have_link('Settings',    href: edit_user_path(user)) }
+    # it { should have_link('Settings',    href: edit_user_path(user)) }
    	it { should have_link('Sign out',    href: signout_path) }
    	it { should_not have_link('Sign in', href: signin_path) }
 
@@ -107,7 +107,7 @@ describe "Authentication" do
         end
 
         describe "submitting to the destroy action" do
-          before { delete microposts_path(FactoryGirl.create(:micropost)) }
+          before { delete micropost_path(FactoryGirl.create(:micropost)) }
           specify { expect(response).to redirect_to(signin_path) }
         end
 
